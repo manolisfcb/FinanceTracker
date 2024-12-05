320a603105851746e020f0fd415af236c371cf38
manolisfcb <mmedinac26@gmail.com>
2024-12-04 21:26:59 -0300

    Add user registration form, update authentication routes, and enhance templates with flash messages

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,10 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 from .main import main_bp
 from flask_login import login_user, logout_user, login_required, current_user, UserMixin, LoginManager
+from src.forms.UserRegistratioForm import NamerForm
+from flask import Blueprint, render_template, request
 
-@main_bp.route('/register', methods=['GET'])
+@main_bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    name = None
+    password = None
+    email = None
+    form = NamerForm()
+
+    # Maneja solicitudes POST (cuando el formulario es enviado)
+    if request.method == 'POST' and form.validate_on_submit():
+        name = form.name.data
+        password = form.password.data
+        email = form.email.data
+        form.name.data = ''  # Limpia el campo después de procesar
+        flash(f'Account created for {name}!', 'success')
+        return render_template('register.html', name=name, form=form)
+
+    # Maneja solicitudes GET (muestra el formulario inicialmente)
+    return render_template('register.html', name=None, form=form)
+
 
 
 @main_bp.route('/login', methods=['GET'])
