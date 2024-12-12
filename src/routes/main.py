c0897c9d171803ae11386c9531be7576c304a832
manolisfcb <mmedinac26@gmail.com>
2024-12-11 23:55:01 -0300

    Remove unnecessary whitespace in home_page route

--- src/routes/main.py
+++ src/routes/main.py
@@ -4,5 +4,4 @@ main_bp = Blueprint('main', __name__, url_prefix='/')
 
 @main_bp.route('/', methods=['GET'])
 def home_page():
-    return render_template('index.html')                                                                                                                                                               hackea la compu
-
+    return render_template('index.html')
