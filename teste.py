d227dc5b9ad59dad7ad26d63c47c607b469f9be6
manolisfcb <mmedinac26@gmail.com>
2025-11-15 11:14:04 -0500

    .

--- teste.py
+++ teste.py
@@ -1,20 +1,45 @@
-import requests
+from abc import ABC, abstractmethod
 
-# URL que apuntaste
-url = "https://www.rad.cvm.gov.br/ENET/frmExibirArquivoIPEExterno.aspx?NumeroProtocoloEntrega=1363564"
+class Mensaje(ABC):
+    def __init__(self, lang):
+        self.lang = lang
+    
+    @abstractmethod
+    def create_message(self, key):
+        pass
 
-# Headers simulando navegador (algunos servidores los requieren)
-headers = {
-    "User-Agent": "Mozilla/5.0"
-}
+class WarningPt(Mensaje):
+    LINKERROR = "Link inválido"
+    LIMITEXCEEDED = "Limite de tentativas excedido"
+    INVALIDPARAMETER = "Parâmetro inválido"
 
-# Hacemos la solicitud
-response = requests.get(url, headers=headers)
-print(response)
-# Verificamos que la respuesta sea exitosa y contenga un PDF
-if response.status_code == 200 and 'application/pdf' in response.headers.get('Content-Type', ''):
-    with open("arquivo_cvm.pdf", "wb") as f:
-        f.write(response.content)
-    print("PDF descargado exitosamente.")
-else:
-    print("No se pudo descargar el PDF. Status:", response.status_code)
+class WarningEs(Mensaje):
+    LINKERROR = "Enlace inválido"
+    LIMITEXCEEDED = "Límite de intentos excedido"
+    INVALIDPARAMETER = "Parámetro inválido"
+
+class WarningEn(Mensaje):
+    LINKERROR = "Invalid link"
+    LIMITEXCEEDED = "Limit exceeded"
+    INVALIDPARAMETER = "Invalid parameter"
+
+class warnings():
+    def __init__(self, lang):
+        self.lang = lang
+        self.WARNINGS = self.get_warning(lang)
+        
+    def get_warning(self, key):
+        return WARNING[self.lang]
+
+
+
+class messenger():
+    def __init__(self, lang):
+        self.lang = lang
+        self.WARNINGS = warnings(lang)
+        # self.error = Errors(lang)
+        # self.info = Info(lang)
+        
+mensaje = Mensaje("es")
+print(mensaje.lang)
+print(mensaje.WARNINGS.warnings)  # {"warning": "Aviso", "warnings":
